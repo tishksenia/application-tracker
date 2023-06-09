eccd36933162575f5c3233d2fb6984f7ddd64354
Ksenia Tishchenko <k.tishchenkox@gmail.com>
2023-06-09 19:41:46 +0400

    add navigation

--- src/shared/ui/layout.tsx
+++ src/shared/ui/layout.tsx
@@ -1,10 +1,17 @@
 import { Inter } from "next/font/google";
+import { Navigation } from "./navigation";
+import { clsx } from "clsx";
 import "app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className={inter.className}>{children}</div>;
+  return (
+    <div className={clsx(inter.className, "md:p-10 p-5")}>
+      <Navigation />
+      {children}
+    </div>
+  );
 };
 
 export { RootLayout };
---
+++ src/shared/ui/navigation.tsx
@@ -0,0 +1,58 @@
+import Link from "next/link";
+import React from "react";
+
+export const Navigation = () => {
+  return (
+    <div className="navbar rounded-lg bg-base-100 mb-10">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            {burgerIcon}
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <Menu />
+          </ul>
+        </div>
+        <Link href="/" className="btn btn-ghost normal-case text-xl">
+          Application Tracker
+        </Link>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <Menu />
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const burgerIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-5 w-5"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M4 6h16M4 12h8m-8 6h16"
+    />
+  </svg>
+);
+
+const Menu = () => (
+  <>
+    <li>
+      <Link href="/">Dashboard</Link>
+    </li>
+    <li>
+      <Link href="/category/1">1</Link>
+    </li>
+  </>
+);
